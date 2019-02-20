cd21f6465a0b08fa0c2d4d307ed9a3b997374496
Yosuke Kobayashi <yosuke@epx.phys.tohoku.ac.jp>
2019-02-21 04:08:12 +0900

    first init

---
+++ setup.sh
@@ -0,0 +1,2 @@
+export CAINTOP=/home/belle2/ykoba/soft/CAIN/cain243
+export PATH=$PATH:$CAINTOP/bin
---
+++ sub.sh
@@ -0,0 +1,1 @@
+bsub -o sub.log -e sub.err -J beam_250 "( /home/belle2/ykoba/soft/CAIN/cain243/bin/runcain.sh beam_250 )"
